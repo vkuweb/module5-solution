b940060f9aad3230080179e3aeb640de2c87f3a8
vkuweb <106548035+vkuweb@users.noreply.github.com>
2025-12-07 15:11:39 +0700

    ajax-utils.js

--- js/ajax-utils.js
+++ js/ajax-utils.js
@@ -1,67 +1,38 @@
-(function (global) {
+(function (window) {
+  var ajaxUtils = {};
 
-// Set up a namespace for our utility
-var ajaxUtils = {};
-
-
-// Returns an HTTP request object
-function getRequestObject() {
-  if (window.XMLHttpRequest) {
-    return (new XMLHttpRequest());
-  } 
-  else if (window.ActiveXObject) {
-    // For very old IE browsers (optional)
-    return (new ActiveXObject("Microsoft.XMLHTTP"));
-  } 
-  else {
-    global.alert("Ajax is not supported!");
-    return(null); 
+  function getRequestObject() {
+    if (window.XMLHttpRequest) {
+      return (new XMLHttpRequest());
+    } else if (window.ActiveXObject) {
+      return (new ActiveXObject("Microsoft.XMLHTTP"));
+    } else {
+      window.alert("Ajax not supported");
+      return null;
+    }
   }
-}
 
-
-// Makes an Ajax GET request to 'requestUrl'
-ajaxUtils.sendGetRequest = 
-  function(requestUrl, responseHandler, isJsonResponse) {
+  ajaxUtils.sendGetRequest = function (requestUrl, responseHandler, isJsonResponse) {
     var request = getRequestObject();
-    request.onreadystatechange = 
-      function() { 
-        handleResponse(request, 
-                       responseHandler,
-                       isJsonResponse); 
-      };
+    request.onreadystatechange = function () {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          if (isJsonResponse === undefined) {
+            isJsonResponse = true;
+          }
+          if (isJsonResponse) {
+            responseHandler(JSON.parse(request.responseText));
+          } else {
+            responseHandler(request.responseText);
+          }
+        } else {
+          console.error("Request error", request.status, request.statusText, request.responseURL);
+        }
+      }
+    };
     request.open("GET", requestUrl, true);
-    request.send(null); // for POST only
+    request.send(null);
   };
 
-
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
-function handleResponse(request,
-                        responseHandler,
-                        isJsonResponse) {
-  if ((request.readyState == 4) &&
-     (request.status == 200)) {
-
-    // Default to isJsonResponse = true
-    if (isJsonResponse == undefined) {
-      isJsonResponse = true;
-    }
-
-    if (isJsonResponse) {
-      responseHandler(JSON.parse(request.responseText));
-    }
-    else {
-      responseHandler(request.responseText);
-    }
-  }
-}
-
-
-// Expose utility to the global object
-global.$ajaxUtils = ajaxUtils;
-
-
+  window.$ajaxUtils = ajaxUtils;
 })(window);
-
